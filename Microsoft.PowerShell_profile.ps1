e3dccafc9055343cd9a1008a9b6f029733281177
NIcholas Pitt <51830202+WelshKaibigan@users.noreply.github.com>
2024-05-09 16:22:00 +0100

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -249,7 +249,7 @@ if (Get-Command zoxide -ErrorAction SilentlyContinue) {
 Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineOption -PredictionViewStyle ListView
 neofetch
-$in = Read-Host Are you doing Dev?
+$in = Read-Host Are you doing Devy?
 if ($in -eq "y") {
  Set-Location -Path J:
  }
